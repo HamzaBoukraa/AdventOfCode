4e58418b899d2a45d6de8ab3b172ab830a776b6d
HamzaBoukraa <pro.hamza.boukraa@gmail.com>
2020-12-13 15:36:37 +0100

    Added Files

--- 2020/Python/day04/AOC2020D04.py
+++ 2020/Python/day04/AOC2020D04.py
@@ -1,16 +1,17 @@
-with open('day4_input.txt', 'r') as reader:
+year = '2020'
+day = '04'
+
+with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as reader:
     passports = [passport for passport in [{key: value for key, value in [field.split(':') for field in line.strip().replace('\n', ' ').split(' ')]} for line in reader.read().split('\n\n')]]
 
-# Part 1 :
+# Part 1 : 
 valid_passports = [passport for passport in passports if len(passport) == 8 or (len(passport) == 7 and not 'cid' in passport)]
+print('Part 1 answer :', len(valid_passports))
 
-print('Valid passport (part 1):', len(valid_passports))
-
-# Part 2 :
+# Part 2 : 
 import re
 regex_hcl = re.compile(r'#[a-f0-9]{6}\Z')
 regex_pid = re.compile(r'[0-9]{9}\Z')
 
 valid_passports = [passport for passport in valid_passports if int(passport['byr']) >= 1920 and int(passport['byr']) <= 2002 and int(passport['iyr']) >= 2010 and int(passport['iyr']) <= 2020 and int(passport['eyr']) >= 2020 and int(passport['eyr']) <= 2030 and ((passport['hgt'][-2:] == 'cm' and int(passport['hgt'][:-2]) >= 150 and int(passport['hgt'][:-2]) <= 193) or (passport['hgt'][-2:] == 'in' and int(passport['hgt'][:-2]) >= 59 and int(passport['hgt'][:-2]) <= 76)) and regex_hcl.match(passport['hcl']) and passport['ecl'] in ['amb','blu','brn','gry','grn','hzl','oth'] and regex_pid.match(passport['pid'])]
-
-print('Valid passport (part 2):', len(valid_passports))
+print('Part 2 answer :', len(valid_passports))
--- 2020/Python/day05/AOC2020D05.py
+++ 2020/Python/day05/AOC2020D05.py
@@ -1,8 +1,11 @@
-with open('AOC2020D05_input.txt', 'r') as reader:
-    seats = [int(s[:-3], 2) * 8 + int(s[-3:], 2) for s in [seat.replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0') for seat in reader.read().split('\n')]]
+year = '2020'
+day = '05'
+
+with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as reader:
+    seats = [int(s[:-3], 2) * 8 + int(s[-3:], 2) for s in [seat.strip().replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0') for seat in reader.read().split('\n')]]
 
 # Part 1 :
-print(max(seats))
+print('Part 1 answer :', max(seats))
 
 # Part 2 :
-print('Missing seat is :', [s for s in range(min(seats), max(seats)) if s not in seats])
+print('Part 2 answer :', [s for s in range(min(seats), max(seats)) if s not in seats][0])
