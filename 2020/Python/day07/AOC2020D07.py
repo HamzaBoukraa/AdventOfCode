6600c46d9a7b6034e9fba12b72e32cb03c806212
HamzaBoukraa <pro.hamza.boukraa@gmail.com>
2020-12-14 15:55:44 +0100

    Added D07

--- 2020/Python/day07/AOC2020D07.py
+++ 2020/Python/day07/AOC2020D07.py
@@ -6,8 +6,6 @@ with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as r
 
 empty_bag = 'other bag'
 my_bag = 'shiny gold bag'
-my_bag = 'dark blue bag'
-# my_bag = 'dark green bag'
 
 # Part 1 :
 bags_list = []
@@ -23,22 +21,13 @@ while added_bags > 0:
 print('Part 1 answer :', len(bags_list))
 
 # Part 2 :
-def number_of_bags(multiplier, requested_bag):
+def number_of_bags(multiplier, requested_bag, level):
     for bag in [bag_candidate for bag_candidate in bags if bag_candidate[0] == requested_bag]:
+        bags_count = 0
         for contained_bag in bag[1]:
-            print(bag[0], contained_bag[1])
-
-    # if requested_bag == empty_bag:
-    #     print('empty bag')
-    #     return 0
-    # else:
-    #     for bag in [bag_candidate for bag_candidate in bags if bag_candidate[0] == requested_bag]:
-    #         bags_count = 1
-    #         for contained_bag in bag[1]:
-    #             # print(contained_bag[1], contained_bag[0])
-    #             print(multiplier * int(contained_bag[1]), contained_bag[0])
-    #             bags_count += multiplier * number_of_bags(int(contained_bag[1]), contained_bag[0])
-    #         print(bags_count, bag[0])
-    #     return bags_count
-
-number_of_bags(1, my_bag)
+            if contained_bag[0] == empty_bag:
+                return multiplier
+            else:
+                bags_count += multiplier * number_of_bags(int(contained_bag[1]), contained_bag[0], level + 1)
+        return bags_count + multiplier
+print('Part 2 answer :', number_of_bags(1, my_bag, 1) - 1)
