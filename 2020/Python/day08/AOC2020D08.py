94bb6d6b62afe0590d52d46c914b874409506932
HamzaBoukraa <pro.hamza.boukraa@gmail.com>
2020-12-22 12:19:00 +0100

    Commit D08

--- 2020/Python/day08/AOC2020D08.py
+++ 2020/Python/day08/AOC2020D08.py
@@ -2,13 +2,13 @@ year = '2020'
 day = '08'
 
 with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as reader:
-    puzzle_input = [[instruction[0], instruction[1]] for instruction.split(' ') in [input_line for input_line in reader.read().split('\n')]]
+    puzzle_input = [input_line for input_line in reader.read().split('\n')]
 
 steps_part1 = [0 for s in range(len(puzzle_input))]
 
 def process_step(program, step, accumulator, steps):
-    if steps[step] == 0:
-        print(step, program[step])
+    if step < len(program) and step < len(steps) and steps[step] == 0:
+        # print(step, program[step], accumulator)
         steps[step] += 1
         instruction = program[step].split(' ')
         if instruction[0] == 'nop':
@@ -18,13 +18,32 @@ def process_step(program, step, accumulator, steps):
         elif instruction[0] == 'jmp':
             return process_step(program, step + int(instruction[1]), accumulator, steps)
     else:
-        print('Already visited :', step, program[step])
-        return accumulator
+        if step >= len(program):
+            # print('End Reached :', step)
+            return (accumulator, False, step) 
+        else:
+            # print('Already visited :', step, program[step])
+            return (accumulator, True, step, program[step]) 
 
 # Part 1 :
-print('Part 1 answer :', process_step(puzzle_input, 0, 0, steps_part1))
-
-steps_part2 = [0 for s in range(len(puzzle_input))]
+print('Part 1 answer :', process_step(puzzle_input, 0, 0, steps_part1)[0])
 
 # Part 2 :
-print('Part 2 answer :', process_step(puzzle_input, 0, 0, steps_part2))
+last_changed_index = -1
+step = 0
+cyclic_loop = True
+
+while cyclic_loop and step < len(puzzle_input): 
+    puzzle_input_copy = puzzle_input.copy()
+    steps_part2 = [0 for s in range(len(puzzle_input_copy))]
+    instruction = puzzle_input_copy[step].split(' ')
+    if instruction[0] == 'nop' or instruction[0] == 'jmp':
+        if instruction[0] == 'nop':
+            puzzle_input_copy[step] = 'jmp ' + instruction[1]
+        elif instruction[0] == 'jmp':
+            puzzle_input_copy[step] = 'nop ' + instruction[1]
+
+        processing_results = process_step(puzzle_input_copy, 0, 0, steps_part2)
+        cyclic_loop = processing_results[1]
+    step += 1
+print('Part 2 answer :', processing_results[0])
