588a9ca7bb78cdd3bf651833586fd63ff43834c9
HamzaBoukraa <az-ext-hboukraa@mdwn.onmicrosoft.com>
2021-12-05 20:43:12 +0100

    Adding AOC 2021 03

--- 2021/Python/day03/AOC2021D03.py
+++ 2021/Python/day03/AOC2021D03.py
@@ -1,38 +1,65 @@
-year = '2020'
+year = '2021'
 day = '03'
 
 with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as reader:
     puzzle_input =  [map_row for map_row in reader.read().split('\n')]
 
-tree = "#"
-moves = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
-
-def process(move_index):
-    move = moves[move_index]
+gamma_rate = ""
+epsilon_rate = ""
+for pos in range(len(puzzle_input[0])):
+    zeroes = 0
+    ones = 0
+    for input in puzzle_input:
+        if input[pos] == "0":
+            zeroes += 1
+        elif input[pos] == "1":
+            ones += 1
+    if zeroes > ones:
+        gamma_rate += "0"
+        epsilon_rate += "1"
+    else:
+        gamma_rate += "1"
+        epsilon_rate += "0"
     
-    down_progression = move[0]
-    right_progression = move[1]
-    
-    cell_index = [0,0]
-    
-    trees_count = 0
-    
-    while cell_index[0] < len(puzzle_input) - 1:
-        cell_index[1] += right_progression
-        cell_index[0] += down_progression
-        while len(puzzle_input[cell_index[0]]) < cell_index[1]:
-            puzzle_input[cell_index[0]] += puzzle_input[cell_index[0]]
-        position = puzzle_input[cell_index[0]][cell_index[1]]
-        if position == tree:
-            trees_count += 1
-    
-    return trees_count
-
 # Part 1 : 
-print('Part 1 answer :', process(1))
+print('Part 1 answer :', int(gamma_rate,2)*int(epsilon_rate,2))
 
 # Part 2 :
-result = 1
-for step in range(5):
-    result *= process(step)
-print('Part 2 answer :', result)
+puzzle_oxygen = puzzle_input.copy()
+puzzle_co2 = puzzle_input.copy()
+while len(puzzle_oxygen)>1 or len(puzzle_co2)>1:
+    for pos in range(len(puzzle_input[0])):
+
+        zeroes_o = 0
+        ones_o = 0
+
+        for input in puzzle_oxygen:
+            if input[pos] == "0":
+                zeroes_o += 1
+            elif input[pos] == "1":
+                ones_o += 1
+
+        zeroes_co = 0
+        ones_co = 0
+
+        for input in puzzle_co2:
+            if input[pos] == "0":
+                zeroes_co += 1
+            elif input[pos] == "1":
+                ones_co += 1
+
+        puzzle_index = 0
+        while puzzle_index < len(puzzle_oxygen) and len(puzzle_oxygen) > 1:
+            if (zeroes_o > ones_o and puzzle_oxygen[puzzle_index][pos] == "1") or (zeroes_o <= ones_o and puzzle_oxygen[puzzle_index][pos] == "0"):
+                puzzle_oxygen.pop(puzzle_index)
+                puzzle_index -= 1
+            puzzle_index += 1
+
+        puzzle_index = 0
+        while puzzle_index < len(puzzle_co2) and len(puzzle_co2) > 1:
+            if (zeroes_co > ones_co and puzzle_co2[puzzle_index][pos] == "0") or (zeroes_co <= ones_co and puzzle_co2[puzzle_index][pos] == "1"):
+                puzzle_co2.pop(puzzle_index)
+                puzzle_index -= 1
+            puzzle_index += 1
+
+print('Part 2 answer :', int(puzzle_oxygen[0],2)*int(puzzle_co2[0],2))
