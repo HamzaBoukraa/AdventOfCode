32d24581a0379541512218276e973c65f0829ab8
HamzaBoukraa <az-ext-hboukraa@mdwn.onmicrosoft.com>
2021-12-05 20:42:50 +0100

    Adding AOC 2021 02

--- 2021/Python/day02/AOC2021D02.py
+++ 2021/Python/day02/AOC2021D02.py
@@ -1,11 +1,24 @@
-year = '2020'
+year = '2021'
 day = '02'
 
 with open('..\..\Input\day{1}\AOC{0}D{1}_input.txt'.format(year, day), 'r') as reader:
     puzzle_input =  [password for password in reader.read().split('\n')]
 
 # Part 1 : 
-print('Part 1 answer :', len([p for p in [(password, len([l for l in password[1] if password[0] == l])) for password in [(input.split(' ')[-2].split(':')[0], input.split(" ")[-1], int(input.split(' ')[0].split('-')[0]), int(input.split(' ')[0].split('-')[1])) for input in puzzle_input]] if p[1] <= p[0][3] and p[1] >= p[0][2]]))
+print('Part 1 answer :', sum([item[1] for item in [[input.split(' ')[0],int(input.split(' ')[1])] for input in puzzle_input] if item[0] == "forward"])*(sum([item[1] for item in [[input.split(' ')[0],int(input.split(' ')[1])] for input in puzzle_input] if item[0] == "down"])-sum([item[1] for item in [[input.split(' ')[0],int(input.split(' ')[1])] for input in puzzle_input] if item[0] == "up"])))
 
-# Part 2 : 
-print('Part 2 answer :', len([password[1] for password in [(input.split(' ')[-2].split(':')[0], input.split(" ")[-1], int(input.split(' ')[0].split('-')[0]), int(input.split(' ')[0].split('-')[1])) for input in puzzle_input] if password[1][password[2] - 1] != password[1][password[3] - 1] and (password[1][password[2] - 1] == password[0] or password[1][password[3] - 1] == password[0])]))
+# Part 2 :
+depth = 0
+horizontal_position = 0
+aim = 0
+for input in puzzle_input:
+    instruction = input.split(" ")[0]
+    command = int(input.split(" ")[1])
+    if instruction == "forward":
+        horizontal_position += command
+        depth += aim * command
+    elif instruction == "down":
+        aim += command
+    elif instruction == "up":
+        aim -= command
+print('Part 2 answer :', horizontal_position * depth)
