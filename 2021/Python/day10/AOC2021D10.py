f3de307cef8da8159c0f98e78a73393bc309b3d9
HamzaBoukraa <az-ext-hboukraa@mdwn.onmicrosoft.com>
2021-12-20 18:58:21 +0100

    AOC2021D10

--- 2021/Python/day10/AOC2021D10.py
+++ 2021/Python/day10/AOC2021D10.py
@@ -20,8 +20,48 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+errors = [[] for line_index in range(len(puzzle_input))]
+
+for line_index in range(len(puzzle_input)):
+    valid = True
+    c = -1
+    check = ""
+    while valid and c < len(puzzle_input[line_index]) - 1:
+        c += 1
+        char = puzzle_input[line_index][c]
+        if char in ["<","{","(","["]:
+            check += char
+        else:
+            if char in [">","}",")","]"]:
+                expected = ""
+                if check[-1] == "{":
+                    expected = "}"
+                elif check[-1] == "[":
+                    expected = "]"
+                elif check[-1] == "(":
+                    expected = ")"
+                elif check[-1] == "<":
+                    expected = ">"
+                if char == expected:
+                    check = check[:-1]
+                else:
+                    valid = False
+    if valid:
+        errors[line_index]=[]
+    else:
+        score=0
+        if char == ")":
+            score=3
+        elif char == "]":
+            score=57
+        elif char == "}":
+            score=1197
+        elif char == ">":
+            score=25137
+        errors[line_index]=[expected, char, score]
+
 # Part 1 :
-print('Part 1 answer :', puzzle_input)
+print('Part 1 answer :', sum([e[2] for e in errors if e != []]))
 
 # Part 2 :
 print('Part 2 answer :', puzzle_input)
