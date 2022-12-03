2080d7b42429aee03d351217c01ba958d81205ea
Hamza Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2022-12-03 17:23:47 +0100

    day03

---
+++ 2022/python/day03/AOC2022D03.py
@@ -0,0 +1,52 @@
+from aocd import get_data
+import os
+
+year = '2022'
+day = '03'
+
+input_directory_folder_path = '..\..\input\day{0}'.format(day)
+input_file_path = '{0}\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
+
+if not os.path.exists(input_directory_folder_path):
+    os.makedirs(input_directory_folder_path)
+
+if not os.path.exists(input_file_path):
+    data = get_data(year=int(year),day=int(day))
+    
+    with open(input_file_path, 'w') as writer:
+        writer.write(data)
+
+# Get Puzzle Input
+with open(input_file_path, 'r') as reader:
+    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
+
+def get_priority(item_type):
+    item_priority = ord(item_type)-96
+    if item_priority < 0:
+        item_priority += 58
+    return item_priority
+
+# Part 1 :
+item_types = []
+for bag in puzzle_input:
+    c1 = bag[:len(bag)//2]
+    c2 = bag[len(bag)//2:]
+    item_type = next(iter(set(sorted(c1)).intersection(set(sorted(c2)))))
+    item_priority = get_priority(item_type)
+    item_types += [[item_type,item_priority]]
+
+print('Part 1 answer :', sum([i[1] for i in item_types]))
+
+
+# Part 2 :
+item_types = []
+temp_bags = []
+for bag in puzzle_input:
+    temp_bags += [bag]
+    if len(temp_bags) == 3:
+        item_type= next(iter(set(sorted(temp_bags[0])).intersection(set(sorted(temp_bags[1]))).intersection(set(sorted(temp_bags[2])))))
+        item_priority = get_priority(item_type)
+        item_types += [[item_type,item_priority]]
+        temp_bags = []
+
+print('Part 2 answer :', sum([i[1] for i in item_types]))
