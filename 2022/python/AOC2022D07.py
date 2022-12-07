e68faf51cf817b7e8ba11837ba3bdcfc8f629910
Hamza Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2022-12-07 15:26:50 +0100

    day07

--- 2022/python/AOC2022D07.py
+++ 2022/python/AOC2022D07.py
@@ -20,8 +20,60 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+
+
+def process_line(instruction_line, current_path):
+    if instruction_line[0] == '$':
+        current_path = process_instruction(instruction_line, current_path)
+    else:
+        current_path = process_folder_content(instruction_line, current_path)
+    
+    return current_path
+
+def process_folder_content(content_line, current_path):
+    if content_line[0] == 'dir':
+        child_directory_path = current_path + content_line[1] + '/'
+        
+        if child_directory_path not in directories:
+            directories[child_directory_path] = 0
+    else:
+        file_path = current_path + content_line[1]
+        
+        directory_weight_path = file_path
+
+        while len(directory_weight_path) > 1:
+            directories[directory_weight_path[:directory_weight_path.rindex('/') + 1]] += int(content_line[0])
+            directory_weight_path = directory_weight_path[:directory_weight_path.rindex('/')]
+           
+    return current_path
+
+def process_instruction(instruction_arguments, current_path):    
+    if instruction_arguments[1] == 'cd':
+        if instruction_arguments[2] == '/':
+            current_path += '/'
+        elif instruction_arguments[2] == '..':
+            current_path = '/'.join(current_path.split('/')[:-2]) + '/'
+        else:
+            current_path += instruction_arguments[2] + '/'
+        
+        if current_path not in directories:
+            directories[current_path] = 0
+    elif instruction_arguments[1] == 'ls':
+        pass
+    
+    return current_path
+
+directories = {}
+directory_path = ""
+
+input_position = 0
+while input_position < len(puzzle_input):
+    directory_path = process_line(puzzle_input[input_position].split(' '), directory_path)
+    input_position += 1
+
 # Part 1 :
-print('Part 1 answer :')
+print('Part 1 answer :', sum([directories[directory] for directory in directories if directories[directory] <= 100000]))
 
 # Part 2 :
-print('Part 2 answer :')
+required_space = 30000000 - (70000000 - [directories[directory] for directory in directories if directory == '/'][0])
+print('Part 2 answer :', min([directories[directory] for directory in directories if directories[directory] >= required_space]))
