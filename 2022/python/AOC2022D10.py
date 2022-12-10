595f87d3c5d373a0c528a22f1d0caa827e2f6cd0
Hamza Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2022-12-10 21:55:08 +0100

    day10

--- 2022/python/AOC2022D10.py
+++ 2022/python/AOC2022D10.py
@@ -21,7 +21,47 @@ with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
 # Part 1 :
-print('{0}{1} - Part 1 answer : {2}'.format(year, day, 0))
+x = 1
+cycle = 1
+signal_strength = 0
+cycles_strength = [20 + (s * 40) for s in range(6)]
+for instruction_line in puzzle_input:
+    instructions = instruction_line.split(' ')
+    if len(instructions) == 1:
+        cycle += 1
+        if cycle in cycles_strength:
+            signal_strength += x * cycle
+    else:
+        cycle += 1
+        if cycle in cycles_strength:
+            signal_strength += x * cycle
+        
+        x += int(instructions[1])
+        cycle += 1
+        if cycle in cycles_strength:
+            signal_strength += x * cycle
+print('{0}{1} - Part 1 answer : {2}'.format(year, day, signal_strength))
 
 # Part 2 :
-print('{0}{1} - Part 2 answer : {2}'.format(year, day, 0))
+x = 1
+cycle = 0
+screen_sprites = [['.' for s in range(40)] for l in range(6)]
+for instruction_line in puzzle_input:
+    instructions = instruction_line.split(' ')
+    
+    if len(instructions) == 1:
+        cycle += 1
+        if ((cycle - 1)  % 40) in range(x - 1, x + 2):
+            screen_sprites[(cycle - 1) // 40][(cycle - 1) % 40] = '#'
+    else:
+        cycle += 1
+        if ((cycle - 1)  % 40) in range(x - 1, x + 2):
+            screen_sprites[(cycle - 1) // 40][(cycle - 1) % 40] = '#'
+        
+        cycle += 1
+        if ((cycle - 1)  % 40) in range(x - 1, x + 2):
+            screen_sprites[(cycle - 1) // 40][(cycle - 1)  % 40] = '#'
+        x += int(instructions[1])
+print('{0}{1} - Part 2 answer : '.format(year, day))
+for line in screen_sprites:
+    print(''.join([p for p in line]))
