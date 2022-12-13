f0b942b9def8cf51bb69b9f8375945932494fdc0
Hamza Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2022-12-13 23:11:38 +0100

    Advent Of Code 2022D13

--- 2022/python/AOC2022D12.py
+++ 2022/python/AOC2022D12.py
@@ -20,6 +20,36 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+map_position = []
+row_index = 0
+entry_position = (0,0)
+current_position = (0,0)
+exit_position = (0,0)
+
+path = []
+for map_line in puzzle_input:
+    map_row = []
+    column_index = 0
+
+    for p in map_line:
+        if p == 'S':
+            entry_position = (row_index, column_index)
+            map_row += ['a']
+            current_position = (row_index, column_index)
+            path += []
+        elif p == 'E':
+            exit_position = (row_index, column_index)
+            map_row += ['z']
+        else:
+            map_row += [p]
+        column_index += 1
+    
+    map_position += [map_row]
+    row_index += 1
+
+    print(map_row, entry_position, exit_position)
+if map_position[entry_position[0] + 1][entry_position[1]] - map_position[entry_position[0]][entry_position[1]] == 1:
+    path += [()]
 # Part 1 :
 print('{0}{1} - Part 1 answer : {2}'.format(year, day, 0))
 
--- 2022/python/AOC2022D13.py
+++ 2022/python/AOC2022D13.py
@@ -1,5 +1,5 @@
 from aocd import get_data
-import os
+import os, ast, time
 
 year = '2022'
 day = '13'
@@ -20,8 +20,67 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+def compare_items(left_item, right_item):   
+    if len(left_item) > 0 and len(right_item) == 0:
+        return 'F'
+    elif len(left_item) == 0 and len(right_item) > 0:
+        return 'T'
+    elif len(left_item) > 0 and len(right_item) > 0:
+        if isinstance(left_item[0], int) and isinstance(right_item[0], int):
+            if left_item[0] < right_item[0]:
+                return 'T'
+            elif left_item[0] > right_item[0]:
+                return 'F'
+        elif isinstance(left_item[0], list) and isinstance(right_item[0], int):
+            check = compare_items(left_item[0], [right_item[0]])
+            if check in ['T' , 'F']:
+                return check
+        elif isinstance(left_item[0], int) and isinstance(right_item[0], list):
+            check = compare_items([left_item[0]], right_item[0])
+            if check in ['T' , 'F']:
+                return check
+        elif isinstance(left_item[0], list) and isinstance(right_item[0], list):
+            check = compare_items(left_item[0], right_item[0])
+            if check in ['T' , 'F']:
+                return check
+        check = compare_items(left_item[1:], right_item[1:])
+        if check in ['T' , 'F']:
+            return check
+
+signals = []
+left_signal = ''
+right_signal = ''
+signal_strength = 0
+signal_index = 1
+for i in range(len(puzzle_input)):
+    if len(puzzle_input[i]) != 0:
+        signals += [ast.literal_eval(puzzle_input[i])]
+        if left_signal == '':
+            left_signal = ast.literal_eval(puzzle_input[i])
+        else:
+            right_signal = ast.literal_eval(puzzle_input[i])
+            
+            if 'T' == compare_items(left_signal, right_signal):
+                signal_strength += signal_index
+
+            left_signal = ''
+            right_signal = ''
+            signal_index += 1
+
 # Part 1 :
-print('{0}{1} - Part 1 answer : {2}'.format(year, day, 0))
+print('{0}{1} - Part 1 answer : {2}'.format(year, day, signal_strength))
 
 # Part 2 :
-print('{0}{1} - Part 2 answer : {2}'.format(year, day, 0))
+signals += [[[2]]]
+signals += [[[6]]]
+
+for spi in range(len(signals) - 1):
+    for ssi in range(spi, len(signals)):
+        if 'F' == compare_items(signals[spi], signals[ssi]):
+            aux_signal = signals[spi]
+            signals[spi] = signals[ssi]
+            signals[ssi] = aux_signal
+
+s = [s+1 for s in range(len(signals)) if signals[s] == [[2]] or signals[s] == [[6]]]
+
+print('{0}{1} - Part 2 answer : {2}'.format(year, day, s[0] * s[1]))
