66cb1e5b9a97f1fbaf07c492fac12dd3fcff3f4d
hamza.boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2023-12-15 15:30:59 +0100

    AOC2023D06

--- 2023/python/AOC2023D06.py
+++ 2023/python/AOC2023D06.py
@@ -21,9 +21,55 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+from math import ceil, floor
 
-# Part 1 :
-print('{0}{1} - Part 1 answer : {2}'.format(year, day, 1))
+def go_dist(time, button):
+    return button * (time - button)
 
-# Part 2 :
-print('{0}{1} - Part 2 answer : {2}'.format(year, day, 2))
+def part_1():
+    times = [int(x) for x in puzzle_input[0].split(':')[1].split(' ') if x != '']
+    dists = [int(x) for x in puzzle_input[1].split(':')[1].split(' ') if x != '']
+
+    ways_all = 1
+    for x in range(4):
+        ways = 0
+        t = times[x]
+        d = dists[x]
+
+        for button_time in range(t):
+            g = go_dist(t, button_time)
+            if g > d:
+                ways += 1
+        ways_all *= ways
+
+    return ways_all
+
+def part_2():
+    time = int(puzzle_input[0].split(':')[1].replace(' ','').strip())
+    dist = int(puzzle_input[1].split(':')[1].replace(' ','').strip())
+
+    a = -1
+    b = time
+    c = -dist
+
+    coeff = (pow(b, 2) - 4 * a * c) / (4 * a * a)
+
+    sol1 = -b / (2 * a) + pow(coeff, 0.5)
+    sol2 = -b / (2 * a) - pow(coeff, 0.5)
+
+    if sol1 > sol2:
+        sol1, sol2 = sol2, sol1
+
+    sol1 = ceil(sol1)
+    sol2 = floor(sol2)
+
+    assert go_dist(time, sol1) >= dist
+    assert go_dist(time, sol1 - 1) < dist
+    assert go_dist(time, sol2) >= dist
+    assert go_dist(time, sol2 + 1) < dist
+
+    return sol2 - sol1 + 1
+
+if __name__ == '__main__':
+    print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
+    print('{0}{1} - Part 2 answer : {2}'.format(year, day, part_2()))
