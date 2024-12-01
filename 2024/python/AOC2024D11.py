7ea09b417c3a03b1f823942a95e2ae58629a600b
Hamza Leon Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2024-12-01 19:06:40 +0100

    AOCD 2024 skeleton

---
+++ 2024/python/AOC2024D11.py
@@ -0,0 +1,32 @@
+from aocd import get_data
+import os
+
+puzzule_date = __file__.split('\\')[-1].split('.')[0].split('D')
+year = puzzule_date[0][3:]
+day = puzzule_date[1]
+
+input_directory_folder_path = '..\\input'
+input_file_path = '{0}\\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
+
+if not os.path.exists(input_directory_folder_path):
+    os.makedirs(input_directory_folder_path)
+
+if not os.path.exists(input_file_path):
+    data = get_data(year=int(year),day=int(day))
+    
+    with open(input_file_path, 'w') as writer:
+        writer.write(data)
+
+# Get Puzzle Input
+with open(input_file_path, 'r') as reader:
+    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
+
+def part_1():
+    return 0
+
+def part_2():
+    return 0
+
+if __name__ == '__main__':
+    print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
+    print('{0}{1} - Part 2 answer : {2}'.format(year, day, part_2()))
