f62f3567a653cf78bac9d295ed143b353044a65c
Hamza Leon Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2024-12-02 19:01:24 +0100

    AOCD 2015 DAY 02

--- 2015/python/AOC2015D02.py
+++ 2015/python/AOC2015D02.py
@@ -1,11 +1,12 @@
 ﻿from aocd import get_data
 import os
 
-year = '2015'
-day = '02'
+puzzule_date = __file__.split('\\')[-1].split('.')[0].split('D')
+year = puzzule_date[0][3:]
+day = puzzule_date[1]
 
-input_directory_folder_path = '..\input'
-input_file_path = '{0}\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
+input_directory_folder_path = '..\\input'
+input_file_path = '{0}\\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
 
 if not os.path.exists(input_directory_folder_path):
     os.makedirs(input_directory_folder_path)
@@ -20,8 +21,16 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
-# Part 1 :
-print('{0}{1} - Part 1 answer : {2}'.format(year, day, 0))
+boxes_dimensions = [sorted([int(dimension) for dimension in box.split("x")]) for box in puzzle_input]
 
-# Part 2 :
-print('{0}{1} - Part 2 answer : {2}'.format(year, day, 0))
+def part_1():
+    paper_dimensions = [(b[0] * b[1]) + (2 * (b[0] * (b[1] + b[2]) + (b[1] * b[2])))  for b in boxes_dimensions]
+    return sum(paper_dimensions)
+
+def part_2():
+    ribbon_dimensions = [2 * (b[0] + b[1]) + (b[0] * b[1] * b[2])  for b in boxes_dimensions]
+    return sum(ribbon_dimensions)
+
+if __name__ == '__main__':
+    print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
+    print('{0}{1} - Part 2 answer : {2}'.format(year, day, part_2()))
