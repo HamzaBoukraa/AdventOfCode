125806aa4b775e282f3417acc9f77a9875cecab1
Hamza Leon Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2024-12-02 13:01:57 +0100

    AOCD 2024 DAY 02

--- 2024/python/AOC2024D02.py
+++ 2024/python/AOC2024D02.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 import os
+import copy
 
 puzzule_date = __file__.split('\\')[-1].split('.')[0].split('D')
 year = puzzule_date[0][3:]
@@ -21,11 +22,62 @@ if not os.path.exists(input_file_path):
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
 
+reports = [[int(level) for level in report.split()] for report in puzzle_input]
+
+def test_report_safety_part_1(report):
+    report_direction = "init"
+    report_status = "safe"
+
+    for level_index in range(len(report) - 1):
+        if level_index == 0:
+            if report[level_index] < report[level_index + 1]:
+                report_direction = "increase"
+            elif report[level_index] > report[level_index + 1]:
+                report_direction = "decrease"
+            else:
+                report_status = "unsafe"
+
+        if (report_status != "unsafe") and \
+            (((report_direction == "increase") and ((report[level_index] >= report[level_index + 1]) or ((report[level_index] + 3) < report[level_index + 1]))) \
+                or ((report_direction == "decrease") and ((report[level_index] <= report[level_index + 1]) or ((report[level_index] - 3) > report[level_index + 1])))):
+                    report_status = "unsafe"
+
+    return report_status
+
+
+def test_report_safety_part_2(report):
+    report_status = test_report_safety_part_1(report)
+
+    level_to_remove = 0
+    while report_status != "safe" and level_to_remove < len(report):
+        subreport = copy.deepcopy(report)
+        subreport.pop(level_to_remove)
+        report_status = test_report_safety_part_1(subreport)
+        level_to_remove += 1
+
+    return report_status
+
+
 def part_1():
-    return 0
+    safe_reports = 0
+    for report in reports:
+        report_status = test_report_safety_part_1(report)
+
+        if report_status == "safe":
+            safe_reports += 1
+
+    return safe_reports
 
 def part_2():
-    return 0
+    safe_reports = 0
+    for report in reports:
+        report_status = test_report_safety_part_2(report)
+
+        if report_status == "safe":
+            safe_reports += 1
+
+    return safe_reports
+
 
 if __name__ == '__main__':
     print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
