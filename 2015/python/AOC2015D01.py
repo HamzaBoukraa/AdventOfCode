242d2205b1f09e7729c96e7b2b00f1be277e2a51
Hamza Leon Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2024-12-02 19:01:02 +0100

    AOCD 2015 DAY 01

--- 2015/python/AOC2015D01.py
+++ 2015/python/AOC2015D01.py
@@ -1,11 +1,12 @@
 ﻿from aocd import get_data
 import os
 
-year = '2015'
-day = '01'
+puzzule_date = __file__.split('\\')[-1].split('.')[0].split('D')
+year = puzzule_date[0][3:]
+day = puzzule_date[1]
 
-input_directory_folder_path = '..\input'
-input_file_path = '{0}\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
+input_directory_folder_path = '..\\input'
+input_file_path = '{0}\\AOC{1}D{2}_input.txt'.format(input_directory_folder_path,year, day)
 
 if not os.path.exists(input_directory_folder_path):
     os.makedirs(input_directory_folder_path)
@@ -18,10 +19,22 @@ if not os.path.exists(input_file_path):
 
 # Get Puzzle Input
 with open(input_file_path, 'r') as reader:
-    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
+    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')][0]
 
-# Part 1 :
-print('{0}{1} - Part 1 answer : {2}'.format(year, day, 0))
+def part_1():    
+    return len([direction for direction in puzzle_input if direction == '(']) - len([direction for direction in puzzle_input if direction == ')'])
 
-# Part 2 :
-print('{0}{1} - Part 2 answer : {2}'.format(year, day, 0))
+def part_2():
+    position = 0
+    instruction = 0
+    while position != (-1) and instruction < len(puzzle_input):
+        if puzzle_input[instruction] == '(':
+            position += 1
+        elif puzzle_input[instruction] == ')':
+            position -= 1
+        instruction += 1
+    return instruction
+
+if __name__ == '__main__':
+    print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
+    print('{0}{1} - Part 2 answer : {2}'.format(year, day, part_2()))
