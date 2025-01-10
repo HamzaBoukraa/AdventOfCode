6b2b8204014d502614f8c2bd36c69ae2824d52aa
Hamza Leon Boukraa <hamza.boukraa@iteework.onmicrosoft.com>
2025-01-10 17:47:10 +0100

    AOCD 2024

--- 2023/python/AOC2023D10.py
+++ 2023/python/AOC2023D10.py
@@ -20,9 +20,65 @@ if not os.path.exists(input_file_path):
 # Get Puzzle Input
 with open(input_file_path, 'r') as reader:
     puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
+char_swap = {'F': '\u250c', 'J': '\u2518',
+                     '7': '\u2510', 'L': '\u2514',
+                     '-': '\u2500', '|': '\u2502',
+                     'S': 'x',      '@': 'o'}
+import copy
+
+map = []
+for row_index in range(len(puzzle_input)):
+    for column_index in range(len(puzzle_input[row_index])):
+        map += [[[row_index, column_index], puzzle_input[row_index][column_index], [0, 0, 0, 0], -1]]
+        if map[-1][1] == 'L':
+            map[-1][2][0] = map[-1][2][1] = 1
+        elif map[-1][1] == 'J':
+            map[-1][2][0] = map[-1][2][3] = 1
+        elif map[-1][1] == 'F':
+            map[-1][2][1] = map[-1][2][2] = 1
+        elif map[-1][1] == '7':
+            map[-1][2][2] = map[-1][2][3] = 1
+        elif map[-1][1] == '-':
+            map[-1][2][1] = map[-1][2][3] = 1
+        elif map[-1][1] == '|':
+            map[-1][2][0] = map[-1][2][2] = 1
+        elif map[-1][1] == 'S':
+            map[-1][3] = 0
 
 def part_1():
-    return 0
+    current_step = 0
+    start_node = [m for m in map if m[3] == 0]
+    current_nodes = start_node
+    current_step += 1
+    while len(current_nodes) > 0:
+        nodes = copy.deepcopy(current_nodes)
+        current_nodes = []
+        for node in nodes:
+            if node[2][0] == 1 or node[3] == 0:
+                north_node = [m for m in map if m[0][0] == node[0][0] - 1 and m[0][1] == node[0][1] and m[2][2] == 1 and node[3] == current_step - 1 and m[3] == -1]
+                if len(north_node) > 0:
+                    north_node[0][3] = current_step
+                    current_nodes += [north_node[0]]
+            if node[2][2] == 1 or node[3] == 0:
+                south_node = [m for m in map if m[0][0] == node[0][0] + 1 and m[0][1] == node[0][1] and m[2][0] == 1 and node[3] == current_step - 1 and m[3] == -1]
+                if len(south_node) > 0:
+                    south_node[0][3] = current_step
+                    current_nodes += [south_node[0]]
+            if node[2][1] == 1 or node[3] == 0:
+                east_node = [m for m in map if m[0][0] == node[0][0] and m[0][1] == node[0][1] + 1 and m[2][3] == 1 and node[3] == current_step - 1 and m[3] == -1]
+                if len(east_node) > 0:
+                    east_node[0][3] = current_step
+                    current_nodes += [east_node[0]]
+            if node[2][3] == 1 or node[3] == 0:
+                west_node = [m for m in map if m[0][0] == node[0][0] and m[0][1] == node[0][1] - 1 and m[2][1] == 1 and node[3] == current_step - 1 and m[3] == -1]
+                if len(west_node) > 0:
+                    west_node[0][3] = current_step
+                    current_nodes += [west_node[0]]
+            
+        current_step += 1
+
+    return max([m[3] for m in map])
+
 
 def part_2():
     return 0
--- 2024/python/AOC2024D09.py
+++ 2024/python/AOC2024D09.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 import os
+import copy
 
 puzzule_date = __file__.split('\\')[-1].split('.')[0].split('D')
 year = puzzule_date[0][3:]
@@ -19,13 +20,87 @@ if not os.path.exists(input_file_path):
 
 # Get Puzzle Input
 with open(input_file_path, 'r') as reader:
-    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')]
+    puzzle_input =  [puzzle_line for puzzle_line in reader.read().split('\n')][0]
 
+file_blocks = []
+file_id = 0
+block_id = 0
+for block in puzzle_input:
+    if (block_id % 2) == 0:
+        file_blocks += int(block) * [(len(file_blocks), str(file_id), int(block))]
+        file_id += 1
+    else:
+        file_blocks += int(block) * [(len(file_blocks), '.', int(block))]
+    block_id += 1
+    
 def part_1():
-    return 0
+    block_format = copy.deepcopy(file_blocks)
+    
+    block_index = 0
+    while block_index < len(block_format):
+        # print(block_index, ''.join(block_format))
+        # print(block_index, block_format[block_index])
+        if block_format[block_index][1] == ".":
+            exchange_block_index = len(block_format) - 1
+            while exchange_block_index > block_index and block_format[exchange_block_index][1] == ".":
+                exchange_block_index -= 1
+
+            if exchange_block_index != block_index:
+                # print(block_index, exchange_block_index, block_format[block_index], block_format[exchange_block_index])
+                block_format[block_index],block_format[exchange_block_index] = block_format[exchange_block_index],block_format[block_index]
+        block_index += 1
+
+    checksum = 0
+    block_index = 0
+    while block_index < len(block_format) and block_format[block_index][1] != ".":
+        if block_format[block_index][1] != ".":
+            checksum += block_index * int(block_format[block_index][1])
+            # print(block_index, checksum, block_format[block_index])
+        block_index += 1
+
+    return checksum
 
 def part_2():
-    return 0
+    block_format = copy.deepcopy(file_blocks)
+
+    # last_file_id = max([int(p[1]) for p in block_format if p[1] != "."])
+    # print(last_file_id)
+
+    # while last_file_id > 0:
+    #     block_to_move_index = min([i for i in range(len(block_format)) if block_format[i][1] == str(last_file_id)])
+    #     block_size = [int(p[2]) for p in block_format if p[1] == str(last_file_id)][0]
+    #     print(last_file_id, block_to_move_index, block_size)
+        
+    #     temp_block_size = 0        
+    #     free_place_index = -1
+
+    #     temp_free_block_index = 0
+    #     while temp_free_block_index < block_to_move_index and free_place_index == -1:
+    #         if block_format[temp_free_block_index][1] == ".":
+    #             temp_block_size += 1
+    #         else:
+    #             temp_block_size = 0
+            
+    #         if temp_block_size == block_size:
+    #             free_place_index = temp_free_block_index + 1 - block_size
+    #         temp_free_block_index += 1
+    #     if temp_free_block_index < block_to_move_index:
+    #         for i in range(block_size):
+    #             block_format[block_to_move_index + i], block_format[free_place_index + i] = block_format[free_place_index + i], block_format[block_to_move_index + i]
+    #             # print(block_to_move_index, block_format[block_to_move_index])
+    #     last_file_id -= 1
+
+    checksum = 0
+    # block_index = 0
+    # while block_index < len(block_format):
+    #     if block_format[block_index][1] != ".":
+    #         checksum += block_index * int(block_format[block_index][1])
+    #         # print(block_index, checksum, block_format[block_index])
+    #     # print(block_format[block_index], block_index, checksum, ''.join([p[1] for p in block_format]))
+    #     block_index += 1
+
+    return checksum
+
 
 if __name__ == '__main__':
     print('{0}{1} - Part 1 answer : {2}'.format(year, day, part_1()))
